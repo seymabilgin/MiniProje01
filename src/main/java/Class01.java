6d075b424e4dbce346183e7c58fe9202acce2580
John Doe <johndoe@gmail.com>
2023-12-02 21:52:36 +0300

    v2

--- src/main/java/Class01.java
+++ src/main/java/Class01.java
@@ -1,6 +1,14 @@
 public class Class01 {
     //Ben geldim (Ridvan)
     //Güncelleme yapıp aticam simdi bu kodları
+    public static void main(String[] args) {
+        // Bir kenarı 4cm diğer kenarı 5 cm olan dikdörtgenin alanını hesaplayan method.. Guncelleme Ridvan.
+        int result = karesiniBul(4,5);
+        System.out.println(result);
 
+    }public static int karesiniBul(int a, int b){
+        return a*b;
+    }
+    //sevilay 03.12.2023
 
 }
